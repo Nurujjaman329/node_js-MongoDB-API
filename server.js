c0c1fa6e9d9de13345f5b1530666cd24fd4abcc5
Md Nurujjaman <mdnurujjaman329@gmail.com>
2025-08-24 17:35:33 +0600

    fix : PORT issue

--- server.js
+++ server.js
@@ -17,4 +17,9 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/steps", require("./routes/step"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+// Listen on all interfaces
+app.listen(PORT, '0.0.0.0', () =>
+  console.log(`🚀 Server running on port ${PORT}`)
+);
+
